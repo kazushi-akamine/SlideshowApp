e948fae13fbb73b783d05cba048d52110376b5bb
parubro <paruobro@gmail.com>
2019-09-28 22:22:49 +0900

    スライドショーアプリ

---
+++ SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  SlideshowApp
+//
+//  Created by Maitoparta on 2019/09/28.
+//  Copyright © 2019 paruobro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var image1: UIImage!
+   
+    
+    @IBOutlet weak var secondView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       secondView.image = self.image1
+        }
+
+        // Do any additional setup after loading the view.
+       
+        
+    
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,7 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let images = [UIImage(named: "gaikan.jpg"), UIImage(named: "tennai1.jpg"), UIImage(named: "tennai2.jpg")]
+    
+    var imageIndex: Int = 0
+    
+    var timer: Timer!
+    
+    @IBAction func next(_ sender: Any) {
+        if self.timer != nil {
+        } else if imageIndex < 1 {
+            imageIndex += 1
+        } else if imageIndex < 2 {
+            imageIndex += 1
+        } else if imageIndex < 3 {
+            imageIndex -= 2
+        }
+        imageView.image = images[imageIndex]
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        if self.timer != nil {
+        } else if imageIndex == 0 {
+            imageIndex = 2
+        } else {
+            imageIndex -= 1
+        }
+        imageView.image = images[imageIndex]
+    }
+    
+    @IBAction func startStop(_ sender: Any) {
+        if self.timer == nil{
+            self.timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+        } else {
+            timer.invalidate()
+            self.timer = nil
+        }
+    }
+    @objc func updateTimer(_ timer: Timer){
+        if imageIndex == 2 {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        imageView.image = images[imageIndex]
+    }
+    
+    @IBAction func tapImage(_ sender: Any) {
+         performSegue(withIdentifier: "Second", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "Second" {
+            
+            let secondView = segue.destination as! SecondViewController
+            secondView.image1 = images[imageIndex]
+        }
+    }
+   
+    @IBAction func unwind(_ segue: UIStoryboardSegue){
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
